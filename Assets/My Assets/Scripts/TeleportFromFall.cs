3ca2cfe456aff7f030a9917b93e3c8760044781a
Thomas Moffet <pantherman1996@gmail.com>
2015-08-21 19:41:33 +1000

    Did a bit so you can't fall off the level. Also made the level a bit cooler. I also changed the turn speed on the player so it should feel a bit more responsive.

---
+++ Assets/My Assets/Scripts/TeleportFromFall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportFromFall : MonoBehaviour {
+
+	public float yLevel;
+	Transform objTransform;
+	Rigidbody objRigidbody;
+	public Vector3 teleportPosition;
+
+	// Use this for initialization
+	void Start () {
+		objTransform = GetComponent<Transform>();
+		objRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate () {
+		// Debug.Log("Y = " + objTransform.transform.position.y);
+		if (objTransform.transform.position.y < yLevel) {
+			objTransform.transform.position = teleportPosition;
+			objRigidbody.velocity = new Vector3(0,0,0);
+
+		}
+	}
+}
